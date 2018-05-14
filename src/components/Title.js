76be0e14e9a60d36e1bee7fe815a0838935368d9
Michał Matysiak <michalmatysiak02@gmail.com>
2018-05-14 17:43:14 +0200

    Change title.js

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,5 +1,8 @@
 import React from 'react';
-
-const Title = props => <div><h2>{props.title}</h2><p>liczba dodanych zadań: {props.length}</p></div>;
-
+const Title = props => (
+	 <div>
+	 	<h2>{props.title}</h2>
+	 	<p>liczba dodanych zadań: {props.length}</p>
+	 </div>;
+)
 export default Title;
